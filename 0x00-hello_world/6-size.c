8104c1516421d71e98bd79845e18140af0a93933
frankjay1122 <francisbright410@gmail.com>
2023-06-15 16:23:52 -0700

    goodnight

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,32 +3,15 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the size of various types on both
- *              32-bit and 64-bit computers.
- *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("32-bit Computer:\n");
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s)\n", sizeof(double));
-	printf("Size of pointer: %lu byte(s)\n", sizeof(void *));
-
-	printf("\n");
-
-	printf("64-bit Computer:\n");
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s)\n", sizeof(double));
-	printf("Size of pointer: %lu byte(s)\n", sizeof(void *));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
