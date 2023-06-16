6e0f268699c9b687404c18a7b221e39e94492594
frankjay1122 <francisbright410@gmail.com>
2023-06-15 17:02:48 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int length = 59;
+	char *message = "and that piece of art is useful\" - Dora Korpar, "
+			"2015-10-19\n";
+	int length = sizeof(message) - 1;
 
 	write(2, message, length);
 
 	return (1);
 }
-
