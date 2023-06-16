d9aa756fe877fa6a7754a55d88fc9bf7a4d50f10
frankjay1122 <francisbright410@gmail.com>
2023-06-15 21:59:37 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: Always 1
+ * Return: Always 1.
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, "
-			"2015-10-19\n";
-	int length = sizeof(message) - 1;
-
-	write(2, message, length);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
